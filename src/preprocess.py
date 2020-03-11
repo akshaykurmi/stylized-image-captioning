bd0095006267958a62ff93cb4ca817ba9e052369
Akshay Kurmi <akskurmi@gmail.com>
2020-03-11 14:40:41 -0400

    Add threshold to Tokenizer

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,9 +1,11 @@
 import re
 import string
+from collections import Counter
 
 
 class Tokenizer:
-    def __init__(self):
+    def __init__(self, unk_threshold=5):
+        self.unk_threshold = unk_threshold
         self.start, self.end, self.unk, self.pad = "<start>", "<end>", "<unk>", "<pad>"
         self.token_2_index = {self.pad: 0, self.unk: 1, self.start: 2, self.end: 3}
         self.index_2_token = {v: k for k, v in self.token_2_index.items()}
@@ -24,12 +26,12 @@ class Tokenizer:
 
     def fit_on_texts(self, texts):
         texts = [self.preprocess(t) for t in texts]
-        for text in texts:
-            for token in text:
-                if token not in self.token_2_index:
-                    index = self.vocab_size
-                    self.token_2_index[token] = index
-                    self.index_2_token[index] = token
+        counter = Counter([token for text in texts for token in text])
+        for token, count in counter.most_common():
+            if count > self.unk_threshold and token not in self.token_2_index:
+                index = self.vocab_size
+                self.token_2_index[token] = index
+                self.index_2_token[index] = token
 
     def texts_to_sequences(self, texts):
         texts = [self.preprocess(t) for t in texts]
