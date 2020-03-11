8d5cc979c4da1821d40ec7cca24dbe5bf204e75e
Akshay Kurmi <akskurmi@gmail.com>
2020-03-11 14:21:04 -0400

    Add Tokenizer

---
+++ src/preprocess.py
@@ -0,0 +1,47 @@
+import re
+import string
+
+
+class Tokenizer:
+    def __init__(self):
+        self.start, self.end, self.unk, self.pad = "<start>", "<end>", "<unk>", "<pad>"
+        self.token_2_index = {self.pad: 0, self.unk: 1, self.start: 2, self.end: 3}
+        self.index_2_token = {v: k for k, v in self.token_2_index.items()}
+
+    @property
+    def vocab_size(self):
+        return len(self.token_2_index)
+
+    def preprocess(self, text):
+        t = text.lower()
+        t = re.sub(f"([{string.punctuation}])", r" \1 ", t)
+        t = re.sub(r"[^a-zA-Z.?!,;'\"]+", r" ", t)
+        t = re.sub(r"\s+", r" ", t)
+        t = t.strip()
+        t = f"{self.start} {t} {self.end}"
+        t = t.split(" ")
+        return t
+
+    def fit_on_texts(self, texts):
+        texts = [self.preprocess(t) for t in texts]
+        for text in texts:
+            for token in text:
+                if token not in self.token_2_index:
+                    index = self.vocab_size
+                    self.token_2_index[token] = index
+                    self.index_2_token[index] = token
+
+    def texts_to_sequences(self, texts):
+        texts = [self.preprocess(t) for t in texts]
+        return list(map(
+            lambda text: list(map(
+                lambda token: self.token_2_index.get(token, self.token_2_index[self.unk]),
+                text)),
+            texts))
+
+    def sequences_to_texts(self, sequences):
+        return list(map(
+            lambda sequence: list(map(
+                lambda index: self.index_2_token[index],
+                sequence)),
+            sequences))
