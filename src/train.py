847416615f7f15138d6cc082914512ffc9776d6a
Akshay Kurmi <akskurmi@gmail.com>
2020-03-12 00:30:43 -0400

    Refactor generator MLE train loops

--- src/train.py
+++ src/train.py
@@ -11,24 +11,26 @@ logger = logging.getLogger(__name__)
 
 
 @tf.function
-def generator_mle_train_batch(args, encoder, generator, loss_fn, batch):
-    images, captions, _ = batch
+def generator_mle_train_batch(encoder, generator, loss_fn, batch, dsa_lambda):
+    images, captions = batch
     with tf.GradientTape() as tape:
         encoder_output = encoder(images)
         predictions, attention_alphas = generator.train_mle_forward(encoder_output, captions)
         loss = loss_fn(captions, predictions)
-        loss += args.generator_mle_dsa_lambda * tf.reduce_mean(1. - tf.reduce_sum(attention_alphas, axis=1) ** 2)
+        # TODO: Include doubly stochastic attention loss?
+        # loss += dsa_lambda * tf.reduce_mean(1. - tf.reduce_sum(attention_alphas, axis=1) ** 2)
         gradients = tape.gradient(loss, encoder.trainable_variables + generator.trainable_variables)
     return loss, gradients
 
 
 @tf.function
-def generator_mle_val_loss(args, encoder, generator, loss_fn, batch):
-    images, captions, _ = batch
+def generator_mle_val_loss(encoder, generator, loss_fn, batch, dsa_lambda):
+    images, captions = batch
     encoder_output = encoder(images)
     predictions, attention_alphas = generator.train_mle_forward(encoder_output, captions)
     loss = loss_fn(captions, predictions)
-    loss += args.generator_mle_dsa_lambda * tf.reduce_mean(1. - tf.reduce_sum(attention_alphas, axis=1) ** 2)
+    # TODO: Include doubly stochastic attention loss?
+    # loss += dsa_lambda * tf.reduce_mean(1. - tf.reduce_sum(attention_alphas, axis=1) ** 2)
     return loss
 
 
@@ -42,9 +44,11 @@ def generator_mle_train(args, dataset_loader):
 
     logger.info("-- Loading training set")
     train_dataset = dataset_loader.load("train", batch_size=args.generator_mle_batch_size)
+    logger.info("-- Loading validation set")
     val_dataset = dataset_loader.load("val", batch_size=args.generator_mle_batch_size)
     num_train_batches = tf.data.experimental.cardinality(train_dataset).numpy()
 
+    logger.info("-- Initializing models")
     encoder = Encoder()
     generator = Generator(vocab_size=dataset_loader.tokenizer.vocab_size, lstm_units=args.generator_lstm_units,
                           embedding_units=args.generator_embedding_units, lstm_dropout=args.generator_lstm_dropout,
@@ -58,18 +62,19 @@ def generator_mle_train(args, dataset_loader):
     val_summary_writer = tf.summary.create_file_writer(os.path.join(args.log_dir, "val"))
 
     global_step = 0
-    for epoch in args.generator_mle_epochs:
-        logger.info(f"Epoch: {epoch + 1}/{args.generator_mle_epochs}")
+    for epoch in range(args.generator_mle_epochs):
+        logger.info(f"-- Epoch: {epoch + 1}/{args.generator_mle_epochs}")
         for step, train_batch in enumerate(tqdm(train_dataset, total=num_train_batches, desc="Batch", unit="batch")):
-            loss, gradients = generator_mle_train_batch(args, encoder, generator, loss_fn, train_batch)
+            loss, gradients = generator_mle_train_batch(encoder, generator, loss_fn,
+                                                        train_batch, args.generator_mle_dsa_lambda)
             global_step += 1
             optimizer.apply_gradients(zip(gradients, encoder.trainable_variables + generator.trainable_variables))
             if global_step % args.generator_mle_logging_steps == 0:
                 with train_summary_writer.as_default():
                     tf.summary.scalar("generator_mle_loss", loss, step=global_step)
-            if (global_step == args.generator_mle_logging_steps or
-                    global_step % args.generator_mle_validate_steps == 0):
-                losses = [generator_mle_val_loss(args, encoder, generator, loss_fn, val_batch)
+            # TODO: Add global_step == args.generator_mle_logging_steps below?
+            if global_step % args.generator_mle_validate_steps == 0:
+                losses = [generator_mle_val_loss(encoder, generator, loss_fn, val_batch, args.generator_mle_dsa_lambda)
                           for val_batch in val_dataset]
                 with val_summary_writer.as_default():
                     tf.summary.scalar("generator_mle_loss", tf.reduce_mean(losses), step=global_step)
