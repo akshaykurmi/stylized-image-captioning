449e5fc98dc361fa3f13f0145142d61d925bf9a5
Akshay Kurmi <akskurmi@gmail.com>
2020-03-12 15:51:34 -0400

    Refactor main run file

--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ import os
 import argparse
 
 from .datasets import PersonalityCaptions, DatasetLoader
-from .misc import init_logging
 from .train import generator_mle_train
+from .utils import init_logging
 
 args = argparse.Namespace()
 
@@ -16,8 +16,10 @@ args.checkpoints_dir = os.path.join(args.results_dir, args.run_id, "checkpoints"
 args.log_dir = os.path.join(args.results_dir, args.run_id, "logs")
 args.overwrite_checkpoint_dir = False
 
-args.do_train = True
-args.do_eval = True
+args.run_pretrain_generator = True
+args.run_pretrain_discriminator = False
+args.run_adversarial_training = False
+args.run_evaluation = False
 args.seed = 42
 
 args.generator_embedding_units = 512
@@ -31,13 +33,21 @@ args.generator_mle_batch_size = 64
 args.generator_mle_epochs = 20
 args.generator_mle_logging_steps = 1
 args.generator_mle_validate_steps = 1000
+args.generator_mle_checkpoint_steps = 50
 
 init_logging(args.log_dir)
 
-if args.do_train:
-    personality_captions = PersonalityCaptions(args.data_dir)
-    dataset_loader = DatasetLoader(personality_captions)
+personality_captions = PersonalityCaptions(args.data_dir)
+dataset_loader = DatasetLoader(personality_captions)
+
+if args.run_pretrain_generator:
     generator_mle_train(args, dataset_loader)
 
-if args.do_eval:
+if args.run_pretrain_discriminator:
+    pass
+
+if args.run_adversarial_training:
+    pass
+
+if args.run_evaluation:
     pass
