9944c86b09c5db35db7d9f731570fa008a197401
Akshay Kurmi <akskurmi@gmail.com>
2020-03-12 19:57:03 -0400

    Create method to load fake and real data for Discriminator training

--- src/datasets.py
+++ src/datasets.py
@@ -1,13 +1,18 @@
-import io
 import json
+import logging
 import os
 import tarfile
 import urllib.request
 from urllib.error import HTTPError
 
+import io
+import numpy as np
 import tensorflow as tf
-from .preprocess import Tokenizer
 from tqdm import tqdm
+
+from .preprocess import Tokenizer
+
+logger = logging.getLogger(__name__)
 
 
 class PersonalityCaptions:
@@ -69,23 +74,83 @@ class DatasetLoader:
         self.tokenizer = Tokenizer()
         self.tokenizer.fit_on_texts([d["caption"] for d in self.dataset.load("train")])
 
-    def load(self, split, batch_size):
+    def load_generator_dataset(self, split, batch_size):
         data = self.dataset.load(split)
         for d in data:
             d.update({"caption": self.tokenizer.texts_to_sequences([d["caption"]])[0]})
+        # TODO: sort here or not? (efficiency vs. bias during training). Maybe shuffle here.
         data = list(sorted(data, key=lambda d: len(d["caption"])))
         image_paths = tf.convert_to_tensor([d["image_path"] for d in data])
         sequences = tf.ragged.constant([d["caption"] for d in data])
         tf_dataset = tf.data.Dataset.from_tensor_slices((image_paths, sequences))
-        tf_dataset = tf_dataset.map(self._dataset_mapper, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        tf_dataset = tf_dataset.map(self._image_sequence_mapper, num_parallel_calls=tf.data.experimental.AUTOTUNE)
         tf_dataset = tf_dataset.padded_batch(batch_size, padded_shapes=([299, 299, 3], [None]))
         tf_dataset = tf_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
         return tf_dataset
 
+    def load_discriminator_dataset(self, split, batch_size, faking_batch_size, encoder, generator):
+        true_data = self.dataset.load(split)
+        # TODO: is sampling only 1/3 of the data each time enough?
+        # TODO: do this for train, but not for val?
+        true_data = np.random.choice(true_data, size=int(len(true_data) / 3), replace=False)
+        for d in true_data:
+            d.update({
+                "caption": self.tokenizer.texts_to_sequences([d["caption"]])[0],
+                "discriminator_label": 1
+            })
+        logger.info("-- Generating fake data")
+        fake_captions = self._generate_fake_captions(true_data, faking_batch_size, encoder, generator)
+        shuffled_captions = [d["caption"] for d in true_data]
+        np.random.shuffle(shuffled_captions)
+        fake_data, shuffled_data = [], []
+        for d, fake_cap, shuffled_cap in zip(true_data, fake_captions, shuffled_captions):
+            fake_data.append({**d, "caption": fake_cap, "discriminator_label": 0})
+            shuffled_data.append({**d, "caption": shuffled_cap, "discriminator_label": 0})
+        data = true_data + fake_data + shuffled_data
+        np.random.shuffle(data)
+        image_paths = tf.convert_to_tensor([d["image_path"] for d in data])
+        sequences = tf.ragged.constant([d["caption"] for d in data])
+        discriminator_labels = tf.ragged.constant([d["discriminator_label"] for d in data])
+        tf_dataset = tf.data.Dataset.from_tensor_slices((image_paths, sequences, discriminator_labels))
+        tf_dataset = tf_dataset.map(self._image_sequence_label_mapper, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        tf_dataset = tf_dataset.padded_batch(batch_size, padded_shapes=([299, 299, 3], [None], [None]))
+        tf_dataset = tf_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+        return tf_dataset
+
+    def _generate_fake_captions(self, true_data, batch_size, encoder, generator):
+        image_paths = tf.convert_to_tensor([d["image_path"] for d in true_data])
+        d = tf.data.Dataset.from_tensor_slices((image_paths,))
+        d = d.map(self._image_mapper, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        d = d.batch(batch_size)
+        d = d.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+        fake_captions = []
+        num_batches = tf.data.experimental.cardinality(d).numpy()
+        for image_batch in tqdm(d, total=num_batches, desc="Batch", unit="batch"):
+            captions = self._generate_caption(image_batch, encoder, generator).numpy()
+            captions = [c[c != self.tokenizer.pad_id] for c in captions]
+            fake_captions.append(captions)
+        return fake_captions
+
+    @tf.function
+    def _generate_caption(self, image_batch, encoder, generator):
+        encoder_outputs = encoder(image_batch)
+        return generator.generate_caption(encoder_outputs, mode="deterministic", start_id=self.tokenizer.start_id,
+                                          end_id=self.tokenizer.end_id)
+
     @staticmethod
-    def _dataset_mapper(image_path, sequence):
+    def _image_sequence_label_mapper(image_path, sequence, label):
+        img, sequence = DatasetLoader._image_sequence_mapper(image_path, sequence)
+        return img, sequence, label
+
+    @staticmethod
+    def _image_sequence_mapper(image_path, sequence):
+        img = DatasetLoader._image_mapper(image_path)
+        return img, sequence
+
+    @staticmethod
+    def _image_mapper(image_path):
         img = tf.io.read_file(image_path)
         img = tf.image.decode_jpeg(img, channels=3)
         img = tf.cast(img, dtype=tf.float32)
         img = tf.image.resize(img, [299, 299])
-        return img, sequence
+        return img
