01be3c2b59da0003d03f37faa6b86b1988b3dda9
Emmanuel Ojuba <mogbojuba@gmail.com>
2020-03-15 14:55:35 -0400

    Fix typo in maluuba_eval.py

--- src/maluuba_eval.py
+++ src/maluuba_eval.py
@@ -2,4 +2,8 @@ from nlgeval import NLGEval
 nlgeval = NLGEval()  # loads the models
 references = [["The boy is good", "The boy is good yeah"], ["They came to play", "They came to dance"]]
 hypothesis = ["The boy is not good", "They are dancing"]
-metrics_dict = nlgeval.compute_individual_metrics(references, hypothesis)
+metrics_dict = nlgeval.compute_metrics(references, hypothesis)
+metric_ind_dict = nlgeval.compute_individual_metrics(references[0], hypothesis[0])
+
+print(metrics_dict)
+print(metric_ind_dict)
